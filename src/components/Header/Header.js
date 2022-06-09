d6ff21e2d5497d9a097eef35c343d1c858013f8d
John Doe <hamedhasan422421@gmail.com>
2022-06-09 15:05:59 +0300

    mobile menu added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,16 +4,16 @@ import { AiFillHome } from 'react-icons/ai';
 import { FaUserGraduate } from 'react-icons/fa';
 import { AiFillMedicineBox } from 'react-icons/ai';
 import { AiFillContacts } from 'react-icons/ai';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 const Header = () => {
 
     
 
     return (
-        <div>
+        <div className=''>
             <div  className="header">
    
-                <ul>
+                <ul className='hidden lg:block'>
                     <li className='icon-box'>
                 <AiFillHome className='icon'/>
                 <Link to='/' >Home</Link>
@@ -31,7 +31,37 @@ const Header = () => {
                 <Link to='/contact' >Contact</Link>
                     </li>
                 </ul>
+
+             
             </div>
+           <div className='lg:hidden fixed w-full z-40 bottom-0 left-0'>
+           <ul className='py-2 flex items-center justify-evenly bg-slate-900'>
+                <NavLink to='/' className='' >
+                <li className='bg-slate-800 p-4 cursor-pointer hover:bg-amber-500 transition-all rounded-full'>
+               
+                <AiFillHome className='text-white text-xl'/>
+                    </li>
+                </NavLink>
+                <NavLink to='/about' >
+                    <li className='bg-slate-800 p-4 cursor-pointer hover:bg-amber-500 transition-all rounded-full'>
+               
+                <FaUserGraduate className='text-white text-xl'/>
+                    </li>
+                </NavLink>
+                <NavLink to='/portfolio' >
+                    <li className='bg-slate-800 p-4 cursor-pointer hover:bg-amber-500 transition-all rounded-full'>
+                
+                <AiFillMedicineBox  className='text-white text-xl'/>
+                    </li>
+                </NavLink>
+                <NavLink to='/contact' >
+                    <li className='bg-slate-800 p-4 cursor-pointer hover:bg-amber-500 transition-all rounded-full'>
+               
+                <AiFillContacts className='text-white text-xl'/>
+                    </li>
+                </NavLink>
+                </ul>
+           </div>
         </div>
     );
 };
