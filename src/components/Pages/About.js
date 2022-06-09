cb1e77c6be65b15c83bbf8a4b67d760cd4d08101
John Doe <hamedhasan422421@gmail.com>
2022-06-09 12:42:08 +0300

    my portfolio section added

--- src/components/Pages/About.js
+++ src/components/Pages/About.js
@@ -62,7 +62,7 @@ const About = () => {
             <path d="M2 11a1 1 0 011-1h2a1 1 0 011 1v5a1 1 0 01-1 1H3a1 1 0 01-1-1v-5zM8 7a1 1 0 011-1h2a1 1 0 011 1v9a1 1 0 01-1 1H9a1 1 0 01-1-1V7zM14 4a1 1 0 011-1h2a1 1 0 011 1v12a1 1 0 01-1 1h-2a1 1 0 01-1-1V4z" />
             </svg>
 
-                <span className='text-md text-white ml-3'>YEARS OF EXPERIENCE</span>
+                <span className='text-sm text-white ml-3'>YEARS OF EXPERIENCE</span>
             </div>
           </div>
       </div>
--- src/components/Pages/Portfolio.js
+++ src/components/Pages/Portfolio.js
@@ -1,9 +1,157 @@
 import React from 'react';
-
+import portfolio1 from '../../assests/images/portfolio1.png'
+import portfolio2 from '../../assests/images/portfolio2.png'
+import portfolio3 from '../../assests/images/portfolio3.png'
+import portfolio4 from '../../assests/images/portfolio4.png'
 const Portfolio = () => {
+
+    const projects = [
+        {
+            name: 'Manufacture Product ',
+            img: portfolio1,
+          
+            _id: 1
+        },
+        {
+            name: 'Doctors Appointment ',
+            img: portfolio2,
+            description: 'doctors portal with google login feature including a dashboard to manage admin panel Payment Gatway and services where an admin can add or remove a doctor and able to manage all the appointments placed by patients. And also there is a review form to leave review for the clients and it will show up on homepage.',
+            _id: 2
+        },
+        {
+            name: 'Electronics Product',
+            img: portfolio3,
+            description: 'It"s a service based website including an admin panel to add or remove services. Where an admin can easily add or remove services from user. And also there is a myItem page form to leave own product for the clients and it will show up on myItem page.',
+            _id: 3
+        },
+        {
+            name: 'Travel Authentication',
+            img: portfolio4,
+            description: 'Travel Authentication with a google login feature including a Protected route to manage the services where an user can login and register based to manage all the checkout placed by user.',
+            _id: 4
+        },
+    ]
     return (
-        <div>
-            portfolio
+        <div className='bg-[#000000e8] py-10 h-screen '>
+      <div className="container mx-auto">
+      <div className="title-section">
+                <h1>my <span className='text-amber-500'>Portfolio</span></h1>
+                <span className='title-bg'>works</span>
+            </div>
+
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5'>
+           
+                    <div class="wrapper">
+                    <div class="card front-face">
+                       <img src={portfolio1}/>
+                    </div>
+                    <div class="card back-face">
+                  
+                       <div class="info">
+                          <div class="text-md text-center font-bold">
+                          Manufacture Product
+                          </div>
+                         <div className='mt-3'>
+                             <span className=' mr-0 p-1 text-sm'>React Js</span>
+                             <span className='p-1 text-sm'>Firebase</span>
+                             <span className='p-1 text-sm'>Node Js</span> <br />
+                             <span className=' p-1 text-sm'>MongoDB</span>
+                             <span className=' p-1 text-sm '>Express Js</span>
+                         </div>
+                       </div>
+                       <ul>
+               <a href="https://manufacture-development.web.app/" target='_blank'>Live </a>
+               <a href="https://github.com/Hamed-Hasan/assignment-twelve-mongodb-client" target="_blank">Client</a>
+               <a href="https://github.com/Hamed-Hasan/assignment-twelve-mongodb-server" target="_blank">Server</a>
+               
+              
+            </ul>
+                    </div>
+                 </div>
+                 
+                    <div class="wrapper">
+                    <div class="card front-face">
+                       <img src={portfolio2}/>
+                    </div>
+                    <div class="card back-face">
+                  
+                       <div class="info">
+                          <div class="text-md text-center font-bold">
+                          Doctors Appointment
+                          </div>
+                         <div className='mt-3'>
+                             <span className=' mr-0 p-1 text-sm'>React Js</span>
+                             <span className='p-1 text-sm'>Firebase</span>
+                             <span className='p-1 text-sm'>Node Js</span> <br />
+                             <span className=' p-1 text-sm'>MongoDB</span>
+                             <span className=' p-1 text-sm '>Express Js</span>
+                         </div>
+                       </div>
+                       <ul>
+               <a href="https://doctors-appointments.netlify.app/" target='_blank'>Live </a>
+               <a href="https://github.com/Hamed-Hasan/doctors-portal-client" target="_blank">Client</a>
+               <a href="https://github.com/Hamed-Hasan/doctors-portal-server" target="_blank">Server</a>
+               
+              
+            </ul>
+                    </div>
+                 </div>
+                    <div class="wrapper">
+                    <div class="card front-face">
+                       <img src={portfolio3}/>
+                    </div>
+                    <div class="card back-face">
+                  
+                       <div class="info">
+                          <div class="text-md text-center font-bold">
+                          Electronics Product
+                          </div>
+                         <div className='mt-3'>
+                             <span className=' mr-0 p-1 text-sm'>React Js</span>
+                             <span className='p-1 text-sm'>Firebase</span>
+                             <span className='p-1 text-sm'>Node Js</span> <br />
+                             <span className=' p-1 text-sm'>MongoDB</span>
+                             <span className=' p-1 text-sm '>Express Js</span>
+                         </div>
+                       </div>
+                       <ul>
+               <a href="https://electronics-zone.web.app/" target='_blank'>Live </a>
+               <a href="https://github.com/Hamed-Hasan/assignment-eleven-mongodb-client" target="_blank">Client</a>
+               <a href="https://github.com/Hamed-Hasan/assignment-eleven-mongodb-server" target="_blank">Server</a>
+               
+              
+            </ul>
+                    </div>
+                 </div>
+                    <div class="wrapper">
+                    <div class="card front-face">
+                       <img src={portfolio4}/>
+                    </div>
+                    <div class="card back-face">
+                  
+                       <div class="info">
+                          <div class="text-md text-center font-bold">
+                          Travel Authentication
+                          </div>
+                         <div className='mt-3'>
+                             <span className=' mr-0 p-1 text-sm'>React Js</span>
+                             <span className='p-1 text-sm'>Firebase</span>
+                             
+                         </div>
+                       </div>
+                       <ul>
+               <a href="https://electronics-zone.web.app/" target='_blank'>Live </a>
+               <a href="https://github.com/Hamed-Hasan/assignment-ten-firebase" target="_blank">Client</a>
+            
+               
+              
+            </ul>
+                    </div>
+                 </div>
+                 
+               
+            </div>
+      </div>
         </div>
     );
 };
