186971434d3e10b39fd9570d88adca7af72f3013
John Doe <hamedhasan422421@gmail.com>
2022-06-09 13:17:28 +0300

    contact us title added

--- src/components/Pages/Contact.js
+++ src/components/Pages/Contact.js
@@ -2,8 +2,15 @@ import React from 'react';
 
 const Contact = () => {
     return (
-        <div>
-            contact
+        <div className='bg-[#000000e8] py-10 h-screen '>
+      <div className="container mx-auto">
+      <div className="title-section">
+                <h1>GET IN <span className='text-amber-500'>TOUCH</span></h1>
+                <span className='title-bg'>CONTACT</span>
+            </div>
+
+            
+      </div>
         </div>
     );
 };
