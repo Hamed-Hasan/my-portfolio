51c3113d047e6ddba28559a449487a7e85d6329d
John Doe <hamedhasan422421@gmail.com>
2022-06-10 19:44:20 +0300

    project added

--- src/components/Pages/Portfolio.js
+++ src/components/Pages/Portfolio.js
@@ -20,6 +20,8 @@ import gear from '../../assests/images/gear.png'
 import con from '../../assests/images/conv.png'
 import cal from '../../assests/images/cal.png'
 import ph from '../../assests/images/ph.png'
+import sh from '../../assests/images/sh.png'
+
 import { Link } from 'react-router-dom';
 
 const Portfolio = () => {
@@ -479,6 +481,37 @@ const Portfolio = () => {
                        <ul>
                <a href="https://phone-hunter-assignment.netlify.app/">Live </a>
                <a href="https://github.com/Hamed-Hasan/assignment-five-money-moster" target="_blank">Client</a>
+            
+               
+              
+            </ul>
+                    </div>
+                 </div>
+                    <div class="wrapper"
+                      data-aos-duration="4500"
+                      data-aos-easing="ease-in-out"
+                      data-aos="zoom-in">
+                    <div class="card front-face">
+                       <img src={sh}/>
+                    </div>
+                    <div class="card bg-primary back-face">
+                    <img src={sh}/>
+                       <div class="info">
+                          <div class="text-md text-center font-bold text-accent">
+                Super Shop
+                          </div>
+                         <div className='mt-3'>
+                             <span className=' mr-0 p-1 text-sm text-accent'>HTML</span>
+                             <span className='p-1 text-sm text-accent'>CSS</span>
+                             <span className='p-1 text-sm text-accent'>BOOTSTRAP</span>
+                             <span className='p-1 text-sm text-accent'>Javascript</span>
+                      
+                             
+                         </div>
+                       </div>
+                       <ul>
+               <a href="https://my-super-shop.netlify.app/">Live </a>
+               <a href="https://github.com/Hamed-Hasan/assignment-eight-super-shop" target="_blank">Client</a>
             
                
               
